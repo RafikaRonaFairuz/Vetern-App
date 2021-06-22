b2c85df01293c38f3dfd7b90d8ad3f4bcbcf0b05
reyhaiz <67406811+reyhaiz@users.noreply.github.com>
2021-06-23 00:10:50 +0700

    Add files via upload

--- app/src/main/java/com/vetern/ui/loginScreen.java
+++ app/src/main/java/com/vetern/ui/loginScreen.java
@@ -5,12 +5,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.vetern.R;
+import com.vetern.modal.EmailPasswordActivity;
 
 public class loginScreen extends Activity {
 
-	private Button login;
-	private Button daftar;
+	private FirebaseAuth mAuth;
+
+	private Button daftar, login;
+	private EditText TextEmail, TextPassword;
+	private DatabaseReference db_vetern;
+	private String EmailPassowrdActivity;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -18,8 +28,14 @@ public class loginScreen extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.loginscreen);
 
+		mAuth = FirebaseAuth.getInstance();
+
 		login = (Button) findViewById(R.id.login);
 		daftar = (Button) findViewById(R.id.daftar);
+		TextEmail = findViewById(R.id.email);
+		TextPassword = findViewById(R.id.password);
+		db_vetern = FirebaseDatabase.getInstance().getReference("EmailPasswprdActivity");
+		EmailPassowrdActivity = db_vetern.push().getKey();
 
 		daftar.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
@@ -30,10 +46,18 @@ public class loginScreen extends Activity {
 
 		login.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
+				simpanData();
 				Intent nextScreen = new Intent(getApplicationContext(), homeScreen.class);
 				startActivity(nextScreen);
 			}
 		});
+	}
+
+	private void simpanData() {
+		EmailPasswordActivity emailpasswordactivity = new EmailPasswordActivity(TextEmail.getText().toString(),
+				TextPassword.getText().toString());
+
+		db_vetern.child(String.valueOf(TextEmail)).setValue(emailpasswordactivity);
 	}
 }
 	
--- app/src/main/java/com/vetern/ui/registScreen.java
+++ app/src/main/java/com/vetern/ui/registScreen.java
@@ -5,13 +5,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.vetern.R;
+import com.vetern.modal.EmailPasswordActivity;
 
 public class registScreen extends Activity {
 
+	private DatabaseReference db_vetern;
+	private String EmailPassowrdActivity;
+
 	private ImageButton button_back;
 	private Button signup;
+	private EditText TextEmail, TextNama, TextNo_tlp, TextPassword, TextKonfr1;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,9 @@ public class registScreen extends Activity {
 		button_back = (ImageButton) findViewById(R.id.button_back);
 		signup = (Button) findViewById(R.id.signup);
 
+		db_vetern = FirebaseDatabase.getInstance().getReference("EmailPasswprdActivity");
+		EmailPassowrdActivity = db_vetern.push().getKey();
+
 		button_back.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				Intent nextScreen = new Intent(getApplicationContext(), loginScreen.class);
@@ -31,9 +43,17 @@ public class registScreen extends Activity {
 
 		signup.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
+				simpanData();
 				Intent nextScreen = new Intent(getApplicationContext(), loginScreen.class);
 				startActivity(nextScreen);
 			}
 		});
+	}
+	private void simpanData() {
+		EmailPasswordActivity emailpasswordactivity = new EmailPasswordActivity(TextEmail.getText().toString(),
+				TextNama.getText().toString()
+		);
+
+		db_vetern.child(String.valueOf(TextEmail)).setValue(emailpasswordactivity);
 	}
 }
